482d7087e46d264ecc012f6fd708ed300c9a30c6
小白nbfan <>
2026-01-21 17:02:26 +0800

    修复Crawlable.java 接口定义

--- src/main/java/lx/model/Crawlable.java
+++ src/main/java/lx/model/Crawlable.java
@@ -2,6 +2,20 @@ package lx.model;
 
 public interface Crawlable {
 
+    String getTitle();
+
+    String getUrl();
+
+    String getPicUrl();
+
+    String getPrice();
+
+    String getVoted();
+
+    String getComments();
+
+    String getArticleMall();
+
     /**
      * 优惠信息在推送时转化为html表格
      */
